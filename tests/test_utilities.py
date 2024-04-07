5b717bf2843e046eb07d7a6909e4b4d244000047
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-04-07 11:50:54 +0100

    Add tests for the utility print functions

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -4,7 +4,13 @@
 import csv
 import os
 
-from hdx_cli_toolkit.utilities import write_dictionary, censor_secret, read_attributes
+from hdx_cli_toolkit.utilities import (
+    write_dictionary,
+    censor_secret,
+    read_attributes,
+    print_banner,
+    print_table_from_list_of_dicts,
+)
 
 
 ATTRIBUTES_FILE_PATH = os.path.join(os.path.dirname(__file__), "fixtures", "attributes.csv")
@@ -71,3 +77,30 @@ def test_read_attributes_json_list():
     )
 
     assert REFERENCE_ATTRIBUTES == dataset_attributes
+
+
+def test_print_banner(capfd):
+    print_banner("test_action")
+    output, errors = capfd.readouterr()
+
+    assert errors == ""
+    parts = output.split("\n")
+    assert len(parts) == 5
+    for part in parts:
+        if len(part) == 0:
+            continue
+        assert len(part) == 42
+
+
+def test_print_table_from_list_of_dicts(capfd):
+    test_data = [{"column a": "a", "column b": "b", "column c": "c"}]
+    print_table_from_list_of_dicts(test_data)
+
+    output, errors = capfd.readouterr()
+    assert errors == ""
+    parts = output.split("\n")
+    assert len(parts) == 6
+    for part in parts:
+        if len(part) == 0:
+            continue
+        assert len(part) in [29, 31]
